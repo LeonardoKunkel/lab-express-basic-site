4c5e619fd5eea920f7df97e2cb424025cc0bbd50
Leonardo <mr_kunkel@hotmail.com>
2022-01-31 16:52:08 -0600

    Server alive || routes created

---
+++ app.js
@@ -0,0 +1,15 @@
+// 1. Importar
+const express = require('express');
+const app = express();
+
+// 2. Middlewares
+require("dotenv").config()
+app.use(express.static('public'));
+
+// 3. Rutas
+app.use('/', require('./routes/index.routes'))
+
+// 4. Servidor
+app.listen(process.env.PORT, () => {
+    console.log(`It's alive`);
+});
---
+++ routes/index.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/home', (req, res) => {
+    res.send('Hello World!')
+});
+router.get('/about', (req, res) => {
+    res.send('Hello World!')
+});
+router.get('/works', (req, res) => {
+    res.send('Hello World!')
+});
+router.get('/gallery', (req, res) => {
+    res.send('Hello World!')
+});
+
+module.exports = router
